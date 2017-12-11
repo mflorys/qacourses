1ecf6dcf6fb89afb2e9b289c70e0e34fb1cff371
Maciej Florys <Maciej.Florys@totaljobsgroup.com>
2017-12-11 16:59:44 +0100

    Zadanie #2, dodany konstruktor klasy Point

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -11,4 +11,9 @@ public class Point {
         return Math.sqrt(Math.pow((this.x - p2.x),2) + Math.pow(this.y-p2.y,2));
 
     }
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
 }
--- sandbox/src/main/java/test.java
+++ sandbox/src/main/java/test.java
@@ -1,12 +1,8 @@
 public class test{
 	public static void main(String[] args){
 
-		Point p1 = new Point();
-		Point p2 = new Point();
-		p1.x = -4.4;
-		p1.y = -1;
-		p2.x = 2;
-		p2.y = 2.1;
+		Point p1 = new Point(-4.4, -1);
+		Point p2 = new Point(2, 2.1);
 
 		System.out.println(p1.distance(p2));
 		System.out.println(p2.distance(p1));
