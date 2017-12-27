93d87d46fc9265c60fd3365c215a7bc6bbcff9cb
Maciej Florys <Maciej.Florys@totaljobsgroup.com>
2017-12-27 17:32:14 +0100

    Changes in test so that it compares lists' sizes; added getGroupList method

--- addressbook-web-tests/src/test/java/pl/qacourses/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/qacourses/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.qacourses.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
     @Test
@@ -12,13 +14,13 @@ public class GroupModificationTests extends TestBase {
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, "test3"));
         }
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(0);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1edited", "test2edited", "test3edited"));
         app.getGroupHelper().submitGroupModification();
         app.getNavigationHelper().gotoGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(before, after);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(before.size(), after.size());
     }
 }
